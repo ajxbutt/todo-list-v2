1687e2976536023d52d21b0421c37501154627d6
Ubuntu <aj@kiwi.3n022zdmlqnuro4jobwbb0zh2e.zx.internal.cloudapp.net>
2021-11-25 00:16:26 +0000

    backend/frontend application remodeled

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -3,10 +3,12 @@ from application.forms import TaskForm
 from flask import render_template, request, redirect, url_for, jsonify
 import requests
 
+backend_host = "todo-app-backend:5000"
+
 @app.route('/')
 @app.route('/home')
 def home():
-    all_tasks = requests.get("http://todo-app-backend:5000/read/allTasks").json()
+    all_tasks = requests.get(f"http://{backend_host}/read/allTasks").json()
     app.logger.info(f"Tasks: {all_tasks}")
     return render_template('index.html', title="Home", all_tasks=all_tasks["tasks"])
 
@@ -15,7 +17,8 @@ def create_task():
     form = TaskForm()
 
     if request.method == "POST":
-        response = requests.post("http://todo-app-backend:5000/create/task", json={"description": form.description.data})
+        response = requests.post(f"http://{backend_host}/create/task", json={"description": form.description.data})
+        app.logger.info(f"Response: {response.text}")
         return redirect(url_for('home'))
 
     return render_template("create_task.html", title="Add a new Task", form=form)
@@ -23,32 +26,29 @@ def create_task():
 @app.route('/update/task/<int:id>', methods=['GET','POST'])
 def update_task(id):
     form = TaskForm()
-    task = Tasks.query.get(id)
+    task = requests.get(f"http://{backend_host}/read/task/{id}").json()
+    app.logger.info(f"Task: {task}")
 
     if request.method == "POST":
-        task.description = form.description.data
-        db.session.commit()
+        response = requests.put(f"http://{backend_host}/update/task/{id}", json={"description": form.description.data})
         return redirect(url_for('home'))
 
     return render_template('update_task.html', task=task, form=form)
 
-# @app.route('/delete/task/<int:id>')
-# def delete_task(id):
-#     task = Tasks.query.get(id)
-#     db.session.delete(task)
-#     db.session.commit()
-#     return redirect(url_for('home'))
+@app.route('/delete/task/<int:id>')
+def delete_task(id):
+    response = requests.delete(f"http://{backend_host}/delete/task/{id}")
+    app.logger.info(f"Response: {response.text}")
+    return redirect(url_for('home'))
 
-# @app.route('/complete/task/<int:id>')
-# def complete_task(id):
-#     task = Tasks.query.get(id)
-#     task.completed = True
-#     db.session.commit()
-#     return redirect(url_for('home'))
+@app.route('/complete/task/<int:id>')
+def complete_task(id):
+    response = requests.put(f"http://{backend_host}/complete/task/{id}")
+    app.logger.info(f"Response: {response.text}")
+    return redirect(url_for('home'))
 
-# @app.route('/incomplete/task/<int:id>')
-# def incomplete_task(id):
-#     task = Tasks.query.get(id)
-#     task.completed = False
-#     db.session.commit()
-#     return redirect(url_for('home'))
+@app.route('/incomplete/task/<int:id>')
+def incomplete_task(id):
+    response = requests.put(f"http://{backend_host}/incomplete/task/{id}")
+    app.logger.info(f"Response: {response.text}")
+    return redirect(url_for('home'))
