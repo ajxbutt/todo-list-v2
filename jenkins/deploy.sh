b36d7067de43743136198b4d6cbe2ff5fc304bd9
Ubuntu <aj@kiwi.1o5aawszdpmu1cb12ui31cweea.zx.internal.cloudapp.net>
2021-11-28 13:29:11 +0000

    copying over yaml file

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,5 @@
 
 echo "deploy stage"
 
-scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
+scp docker-compose.yaml jenkins@swarm:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm docker stack deploy --compose-file docker-compose.yaml todo-app
